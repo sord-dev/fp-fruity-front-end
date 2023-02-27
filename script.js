d0eb5b8644730489a79bcb560abab24bcbf1947a
Stef <stefansyrett17@gmail.com>
2023-02-27 10:16:58 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const fruitForm = document.querySelector("#input-sect form");
+
+fruitForm.addEventListener("submit", e => {
+    e.preventDefault()
+    let values = {}
+    let inputs = fruitForm.querySelectorAll('input');
+    inputs.forEach(input => values[input.name] = input.value)
+
+    return values;
+})
